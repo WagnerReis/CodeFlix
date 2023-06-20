bbae01cbda030b9b5117927ba212ffcd581b8a74
WagnerReis <wagnerreis1997@gmail.com>
2023-06-19 21:51:51 -0300

    Criando primeiros componentes reutilizáveis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { Box, ThemeProvider, createTheme } from "@mui/material";
+import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
 
-export default function MyApp() {
+const theme = createTheme({});
+
+export default function App() {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        component="main"
+        sx={{
+          height: "100vh",
+          // backgroundColor: "#000",
+        }}
+      >
+        <Header />
+        <Layout>
+          <h1>Ola mundo</h1>
+        </Layout>
+      </Box>
+    </ThemeProvider>
   );
 }
