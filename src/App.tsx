6b011a03e6bbbe8d648d4ed21da0196d38928294
WagnerReis <wagnerreis1997@gmail.com>
2023-06-20 07:05:18 -0300

    Criando tema e adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import { Box, ThemeProvider, createTheme } from "@mui/material";
+import { Box, ThemeProvider, Typography } from "@mui/material";
 import { Header } from "./components/Header";
 import { Layout } from "./components/Layout";
+import { appTheme } from "./config/theme";
+import { Route, Routes } from "react-router-dom";
 
-const theme = createTheme({});
+const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h1" component="h1">
+        Hello
+      </Typography>
+    </Box>
+  );
+};
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={appTheme}>
       <Box
         component="main"
         sx={{
           height: "100vh",
-          // backgroundColor: "#000",
+          backgroundColor: (theme) => theme.palette.grey[900],
         }}
       >
         <Header />
         <Layout>
-          <h1>Ola mundo</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
